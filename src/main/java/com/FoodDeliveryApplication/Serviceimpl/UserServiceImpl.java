16c621c163e995b927d0896e53fff0e4426fd2b4
Rajesh-patale <rajeshpatale888@gmail.com>
2024-07-15 16:46:38 +0530

    changes

--- src/main/java/com/FoodDeliveryApplication/Serviceimpl/UserServiceImpl.java
+++ src/main/java/com/FoodDeliveryApplication/Serviceimpl/UserServiceImpl.java
@@ -19,7 +19,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User fetchUserByEmailAndPassword(String email, String password) {
-        return userRepository.findByEmailAndPassword(email, password);
+        return null;
     }
 
     @Override
